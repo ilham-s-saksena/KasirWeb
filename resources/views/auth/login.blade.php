d2df6b2ffece036ce3a7944dc0cb6394349b965c
Ilham Sidik <ilhamsidikc@gmail.com>
2024-10-15 23:51:10 +0700

    create layouting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
-</head>
-<body>
+@extends('layouts.main')
+
+@section('contents')
 <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -47,5 +41,4 @@
       </div>
   </div>
 </section>
-</body>
-</html>
+@endsection
